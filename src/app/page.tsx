0e0fca79f36f6b41802246227bf640caca823780
Aira San Diego <airasandiego@Airas-MacBook-Air.local>
2026-03-01 13:18:26 -0800

    changed background temp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -78,7 +78,7 @@ import Hero from "./components/Hero"
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-pink-300">
+    <main className="min-h-screen bg-zinc-300">
       <Hero />
     </main>
   );

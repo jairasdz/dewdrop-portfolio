b25b7bdd5a40bed34145471990497156a79f8d1a
Aira San Diego <airasandiego@Airas-MacBook-Air.local>
2026-03-01 13:17:41 -0800

    temp hero card

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,14 +1,14 @@
 export default function Hero() {
     return (
-        <section className="bg-slate-800 flex flex-col items-center justify-center py-32 text-center">
-        <h1 className="text-4xl font-bold tracking-tight">
+        <section className="bg-zinc-50 flex flex-col items-center justify-center py-32 text-center uppercase">
+        <h1 className="text-4xl text-slate-400 font-bold tracking-[0.15em] hover:text-pink-300 transition duration-700">
           Welcome to DewDrop Studios 💧
         </h1>
   
-        <p className="mt-4 max-w-md text-slate-400">
+        <p className="mt-4 max-w-md font-bold text-slate-400">
           wehehehehhe
         </p>
-        <p className="mt-4 max-w-md text-slate-400">
+        <p className="mt-4 max-w-md font-bold text-slate-400">
           just a dewdrop yipee kayay-ing on a pole
         </p>
       </section>        

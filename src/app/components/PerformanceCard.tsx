d858da58951dcde4fc44915c825a1a979a3cc05a
Aira San Diego <airasandiego@Airas-MacBook-Air.local>
2026-03-01 19:44:17 -0800

    added stylings and text to performance card component

--- src/app/components/PerformanceCard.tsx
+++ src/app/components/PerformanceCard.tsx
@@ -7,12 +7,12 @@ type PerformanceCardProps = {
 
 export default function PerformanceCard({ title, date }: PerformanceCardProps) {
     return (
-        <div className="text-center uppercase text-slate-500 font-bold bg-slate-300 rounded-lg">
+        <div className="text-center uppercase text-slate-500 font-bold bg-slate-300 rounded-lg hover:bg-pink-300 transition duration-600">
             <h2>
-                title: {title}
+                {title}
             </h2>
             <p>
-                date: {date}
+                {date}
             </p>
         </div>
     );

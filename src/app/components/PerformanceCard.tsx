f36073916417d2d2bcd577058deaf351ff811beb
Aira San Diego <airasandiego@Airas-MacBook-Air.local>
2026-03-07 00:02:04 -0800

    wip w performance cards

--- src/app/components/PerformanceCard.tsx
+++ src/app/components/PerformanceCard.tsx
@@ -3,17 +3,30 @@ import Link from "next/link"
 type PerformanceCardProps = {
     title: string,
     date: string,
+    image: string
 }
 
-export default function PerformanceCard({ title, date }: PerformanceCardProps) {
+export default function PerformanceCard({ title, date, image }: PerformanceCardProps) {
     return (
-        <div className="text-center uppercase text-slate-500 font-bold bg-slate-300 rounded-lg hover:bg-pink-300 transition duration-600">
-            <h2>
-                {title}
-            </h2>
-            <p>
-                {date}
-            </p>
+        <div className="relative overflow-hidden rounded-xl transition delay-150 duration-300 ease-in-out hover:scale-90">
+            <img
+                src={image}
+                alt={title}
+                loading="lazy"
+                className=""
+            />
+
+            {/* <div className="absolute top-0 w-full bg-slate backdrop-blur-sm p-3">
+                <div className="text-center uppercase text-white font-bold">
+                    <h3>
+                        {title}
+                    </h3>
+                    <p>
+                        {date}
+                    </p>                
+                </div>
+
+            </div> */}
         </div>
     );
 }
--- src/app/performances/page.tsx
+++ src/app/performances/page.tsx
@@ -11,17 +11,20 @@ export default function Performances() {
             <h1 className="flex flex-col items-center font-bold text-center text-xl uppercase tracking-[0.15em]">
                 This is the performances page! Wahooo
             </h1>
-            <section className="mt-8">
+            <section className="mt-4">
                 <h2 className="flex flex-col items-center mb-4 font-bold text-center text-lg uppercase tracking-[0.15em] underline">
                     Upcoming Shows
-                </h2>   
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+                </h2>
+                <h3 className="text-center text-sm font-bold uppercase tracking-[0.15em] mb-4">
+                    (NOTE: All upcoming shows are TBD due to venue issues)
+                </h3>  
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-12">
                     {/* {cards go here } */}
-                    <PerformanceCard title="LOVE IS EVOL" date="TBD"/>
-                    <PerformanceCard title="Gaga for Gaga" date="TBD"/>
-                    <PerformanceCard title="Cabaret Meets Broadway" date="TBD"/>
-                    <PerformanceCard title="Revenge of the Nerdlesque" date="TBD"/>
-                    <PerformanceCard title="Enchanted Ravens Pride" date="TBD"/>
+                    <PerformanceCard title="LOVE IS EVOL" date="TBD" image="images/love-is-evol.JPG"/>
+                    <PerformanceCard title="Gaga for Gaga" date="TBD" image="images/gaga-for-gaga.JPG"/>
+                    <PerformanceCard title="Cabaret Meets Broadway" date="TBD" image="images/broadway-cabaret-night.JPG"/>
+                    <PerformanceCard title="Revenge of the Nerdlesque" date="TBD" image="images/nerdlesque.JPG"/>
+                    <PerformanceCard title="Enchanted Ravens Pride" date="TBD" image="images/er-pride.JPG"/>
 
                 </div>               
             </section>
@@ -29,13 +32,13 @@ export default function Performances() {
                 <h2 className="flex flex-col items-center mb-4 font-bold text-center text-lg uppercase tracking-[0.15em] underline hover:text-slate-500">
                         Past Shows
                 </h2> 
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 ">
-                        <PerformanceCard title="The Twilight Benefit Show" date="January 8, 2026"/>
-                        <PerformanceCard title="The SlutCracker" date="December 4, 2025"/>
-                        <PerformanceCard title="CUNTRY Night" date="November 6, 2025"/>
-                        <PerformanceCard title="OOPS! All Villains" date="October 2, 2025"/>
-                        <PerformanceCard title="Night Of Enchantment" date="August 8, 2025"/>
-                        <PerformanceCard title="Raven Studios 5th Year Anniversary Student Showcase" date="June 28, 2025"/>
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-12">
+                    <PerformanceCard title="The Twilight Benefit Show" date="January 8, 2026" image="images/twilight-benefit-show.JPG"/>
+                    <PerformanceCard title="The SlutCracker" date="December 4, 2025" image="images/slutcracker.JPG"/>
+                    <PerformanceCard title="CUNTRY Night" date="November 6, 2025" image="images/cuntry-night.JPG"/>
+                    <PerformanceCard title="OOPS! All Villains" date="October 2, 2025" image="images/oops-all-villains.JPG"/>
+                    <PerformanceCard title="Night Of Enchantment" date="August 8, 2025" image="images/night-of-enchantment.PNG"/>
+                        {/* <PerformanceCard title="Raven Studios 5th Year Anniversary Student Showcase" date="June 28, 2025"/> */}
                 </div>                
             </section>
         </main>

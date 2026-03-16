b4226777bd98ecaee7cd3b922c6572986ede81db
Aira San Diego <airasandiego@Airas-MacBook-Air.local>
2026-03-16 16:00:10 -0700

    new lib folder for media urls

---
+++ src/lib/performances.ts
@@ -0,0 +1,45 @@
+//performance urls
+
+export const performances = [
+    // upcoming performances
+    {
+        slug: "love-is-evol",
+        title: "LOVE IS EVOL",
+        date: "TBD",
+        category: "upcoming",
+        poster: "https://res.cloudinary.com/df5d8rhg2/image/upload/v1773697789/love-is-evol_grv18i.jpg",
+        media: []
+    },
+    {
+        slug: "gaga-for-gaga",
+        title: "Gaga For Gaga",
+        date: "TBD",
+        category: "upcoming",
+        poster: "https://res.cloudinary.com/df5d8rhg2/image/upload/v1773697789/gaga-for-gaga_avmipl.jpg",
+        media: []
+    },
+    {
+        slug: "broadway-cabaret-night",
+        title: "Broadway Cabaret Night",
+        date: "TBD",
+        category: "upcoming",
+        poster: "https://res.cloudinary.com/df5d8rhg2/image/upload/v1773697789/broadway-cabaret-night_syjyhv.jpg",
+        media: []
+    },
+    {
+        slug: "nerdlesque",
+        title: "Revenge of the Nerdlesque",
+        date: "TBD",
+        category: "upcoming",
+        poster: "https://res.cloudinary.com/df5d8rhg2/image/upload/v1773697789/nerdlesque_ilrzel.jpg",
+        media: []
+    },
+    {
+        slug: "er-pride",
+        title: "Enchanted Ravens Pride",
+        date: "TBD",
+        category: "upcoming",
+        poster: "https://res.cloudinary.com/df5d8rhg2/image/upload/v1773697789/er-pride_sutjts.jpg",
+        media: []
+    },
+]

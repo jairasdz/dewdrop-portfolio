0e8c4ded8d30496b1a130b9a8731b8542f6ecb91
Aira San Diego <airasandiego@Airas-MacBook-Air.local>
2026-03-26 20:14:14 -0700

    cloudinary to imagekit

--- src/lib/performances.ts
+++ src/lib/performances.ts
@@ -6,7 +6,7 @@ export const performances = [
         title: "LOVE IS EVOL",
         date: "TBD",
         category: "upcoming",
-        poster: "https://res.cloudinary.com/df5d8rhg2/image/upload/v1773697789/love-is-evol_grv18i.jpg",
+        poster: "https://ik.imagekit.io/dewypole/love-is-evol/love-is-evol.JPG",
         media: []
     },
     {
@@ -14,7 +14,7 @@ export const performances = [
         title: "Gaga For Gaga",
         date: "TBD",
         category: "upcoming",
-        poster: "https://res.cloudinary.com/df5d8rhg2/image/upload/v1773697789/gaga-for-gaga_avmipl.jpg",
+        poster: "https://ik.imagekit.io/dewypole/gaga-for-gaga/gaga-for-gaga.JPG",
         media: []
     },
     {
@@ -22,7 +22,7 @@ export const performances = [
         title: "Broadway Cabaret Night",
         date: "TBD",
         category: "upcoming",
-        poster: "https://res.cloudinary.com/df5d8rhg2/image/upload/v1773697789/broadway-cabaret-night_syjyhv.jpg",
+        poster: "https://ik.imagekit.io/dewypole/broadway-cabaret-night/broadway-cabaret-night.JPG",
         media: []
     },
     {
@@ -30,7 +30,7 @@ export const performances = [
         title: "Revenge of the Nerdlesque",
         date: "TBD",
         category: "upcoming",
-        poster: "https://res.cloudinary.com/df5d8rhg2/image/upload/v1773697789/nerdlesque_ilrzel.jpg",
+        poster: "https://ik.imagekit.io/dewypole/nerdlesque/nerdlesque.JPG",
         media: []
     },
     {
@@ -38,7 +38,7 @@ export const performances = [
         title: "Enchanted Ravens Pride",
         date: "TBD",
         category: "upcoming",
-        poster: "https://res.cloudinary.com/df5d8rhg2/image/upload/v1773697789/er-pride_sutjts.jpg",
+        poster: "https://ik.imagekit.io/dewypole/er-pride/er-pride.JPG",
         media: []
     },
 
@@ -48,7 +48,7 @@ export const performances = [
         title: "The Twilight Benefit Show",
         date: "January 8, 2026",
         category: "past",
-        poster: "https://res.cloudinary.com/df5d8rhg2/image/upload/v1773697789/twilight-benefit-show_narkhk.jpg",
+        poster: "https://ik.imagekit.io/dewypole/twilight-benefit-show/twilight-benefit-show.JPG",
         media: []
     },
     {
@@ -56,7 +56,7 @@ export const performances = [
         title: "The Slutcracker",
         date: "December 4, 2025",
         category: "past",
-        poster: "https://res.cloudinary.com/df5d8rhg2/image/upload/v1773697789/slutcracker_pzoypg.jpg",
+        poster: "https://ik.imagekit.io/dewypole/the-slutcracker/slutcracker.JPG",
         media: []
     },
     {
@@ -64,7 +64,7 @@ export const performances = [
         title: "CUNTry Night",
         date: "November 6, 2025",
         category: "past",
-        poster: "https://res.cloudinary.com/df5d8rhg2/image/upload/v1773697789/cuntry-night_fqr8db.jpg",
+        poster: "https://ik.imagekit.io/dewypole/cuntry-night/cuntry-night.JPG",
         media: []
     },
     {
@@ -72,7 +72,7 @@ export const performances = [
         title: "Oops! All Villains",
         date: "October 2, 2025",
         category: "past",
-        poster: "https://res.cloudinary.com/df5d8rhg2/image/upload/v1773697789/oops-all-villains_qowgyq.jpg",
+        poster: "https://ik.imagekit.io/dewypole/oops-all-villains/oops-all-villains.JPG",
         media: []
     },
     {
@@ -80,8 +80,14 @@ export const performances = [
         title: "Night of Enchantment",
         date: "August 7, 2025",
         category: "past",
-        poster: "https://res.cloudinary.com/df5d8rhg2/image/upload/v1773697789/night-of-enchantment_s82awy.png",
-        media: []
+        poster: "https://ik.imagekit.io/dewypole/night-of-enchantment/night-of-enchantment.PNG",
+        media: [
+            {
+                src: "https://ik.imagekit.io/dewypole/night-of-enchantment/Night_Of_Enchanment_Dewy.PNG",
+                type: "image",
+                tags: ["promo"]
+            }
+        ]
     },
     // {
     //     slug: "ravens-5th-showcase",

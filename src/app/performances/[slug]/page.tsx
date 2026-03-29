3f5284eddd2484b4a318ea130f307e631c7f6241
Aira San Diego <airasandiego@Airas-MacBook-Air.local>
2026-03-29 13:58:47 -0700

    update slug pages for performances

--- src/app/performances/[slug]/page.tsx
+++ src/app/performances/[slug]/page.tsx
@@ -1,7 +1,19 @@
-import Link from "next/link"
 import { performances } from "@/lib/performances"
+import type { Metadata } from "next";
 
-export default async function PerformanceDetail({params}: { params: Promise<{slug: string}>}) {
+
+//ensures the tab name has the performance name
+export async function generateMetadata({ params }: { params: Promise<{ slug: string }>}): Promise<Metadata> {
+    const { slug } = await params;
+
+    const performance = performances.find((p) => p.slug === slug);
+
+    return {
+        title: performance?.title
+    };
+}
+
+export default async function PerformanceDetail({ params } : { params: Promise<{ slug: string }>}) {
 
     const { slug } = await params;
 
@@ -18,7 +30,7 @@ export default async function PerformanceDetail({params}: { params: Promise<{slu
     }
 
     return (
-        <main className="p-8">
+        <main className="p-8 text-center">
         <h1 className="text-2xl font-bold">{performance.title}</h1>
         <p className="mt-2 text-gray-500">{performance.date}</p>
   

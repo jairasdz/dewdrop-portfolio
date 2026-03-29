7b3629da56e03cee8df88df3de7ea32a4c3a9281
Aira San Diego <airasandiego@Airas-MacBook-Air.local>
2026-03-29 13:42:33 -0700

    created new photoshoots.ts for photoshoot pics

---
+++ src/lib/photoshoots.ts
@@ -0,0 +1,194 @@
+export const photoshoots = [
+    {
+        slug: "street-poles",
+        title: "Street Poles",
+        photographer: "Matt Shaw",
+        dates: "March 2025",
+        socials: [
+            "https://www.mattshawphoto.com/about-matt-shaw-photography",
+            "https://www.instagram.com/mattshawphoto/"
+        ],
+        media: [
+            //SEATTLE CENTER
+            {
+                src: "https://ik.imagekit.io/dewypole/matt-shaw-photoshoot/JUL_8271.jpg",
+                location: "Seattle Center",
+                tags: ["individual"]
+            },
+            {
+                src: "https://ik.imagekit.io/dewypole/matt-shaw-photoshoot/JUL_8275.jpg",
+                location: "Seattle Center",
+                tags: ["individual"]
+            },
+            {
+                src: "https://ik.imagekit.io/dewypole/matt-shaw-photoshoot/JUL_8278.jpg",
+                location: "Seattle Center",
+                tags: ["individual"]
+            },
+            {
+                src: "https://ik.imagekit.io/dewypole/matt-shaw-photoshoot/JUL_8136.jpg",
+                location: "Seattle Center",
+                tags: ["individual"]
+            },
+            {
+                src: "https://ik.imagekit.io/dewypole/matt-shaw-photoshoot/JUL_8152.jpg",
+                location: "Seattle Center",
+                tags: ["individual"]
+            },
+            {
+                src: "https://ik.imagekit.io/dewypole/matt-shaw-photoshoot/JUL_8138.jpg",
+                location: "Seattle Center",
+                tags: ["individual"]
+            },
+            {
+                src: "https://ik.imagekit.io/dewypole/matt-shaw-photoshoot/JUL_8006.jpg",
+                location: "Seattle Center",
+                tags: ["individual"]
+            },
+            {
+                src: "https://ik.imagekit.io/dewypole/matt-shaw-photoshoot/JUL_8128.jpg",
+                location: "Seattle Center",
+                tags: ["individual"]
+            },
+            {
+                src: "https://ik.imagekit.io/dewypole/matt-shaw-photoshoot/JUL_7990.jpg",
+                location: "Seattle Center",
+                tags: ["individual"]
+            },
+            {
+                src: "https://ik.imagekit.io/dewypole/matt-shaw-photoshoot/JUL_7986.jpg",
+                location: "Seattle Center",
+                tags: ["individual"]
+            },
+            {
+                src: "https://ik.imagekit.io/dewypole/matt-shaw-photoshoot/JUL_8283.jpg",
+                location: "Seattle Center",
+                tags: ["individual"]
+            },
+            {
+                src: "https://ik.imagekit.io/dewypole/matt-shaw-photoshoot/65ACFECC-C035-4CA2-94E5-2A4F35A96C66.jpg",
+                location: "Seattle Center",
+                tags: ["group"]
+            },
+
+            //OCCIDENTAL APRK
+            {
+                src: "https://ik.imagekit.io/dewypole/matt-shaw-photoshoot/64EBAE20-1204-49EC-AAAC-2D7F83321907.JPG",
+                location: "Occidental Park",
+                tags: ["individual"]
+            },
+            {
+                src: "https://ik.imagekit.io/dewypole/matt-shaw-photoshoot/JUL_8383.jpg",
+                location: "Occidental Park",
+                tags: ["individual"]
+            },
+            {
+                src: "https://ik.imagekit.io/dewypole/matt-shaw-photoshoot/JUL_8394.jpg",
+                location: "Occidental Park",
+                tags: ["individual"]
+            },
+            {
+                src: "https://ik.imagekit.io/dewypole/matt-shaw-photoshoot/JUL_8385.jpg",
+                location: "Occidental Park",
+                tags: ["individual"]
+            },
+            {
+                src: "https://ik.imagekit.io/dewypole/matt-shaw-photoshoot/JUL_8373.jpg",
+                location: "Occidental Park",
+                tags: ["individual"]
+            },
+            {
+                src: "https://ik.imagekit.io/dewypole/matt-shaw-photoshoot/JUL_8387.jpg",
+                location: "Occidental Park",
+                tags: ["individual"]
+            },
+            {
+                src: "https://ik.imagekit.io/dewypole/matt-shaw-photoshoot/JUL_8463.jpg",
+                location: "Occidental Park",
+                tags: ["individual"]
+            },
+            {
+                src: "https://ik.imagekit.io/dewypole/matt-shaw-photoshoot/JUL_8414.jpg",
+                location: "Occidental Park",
+                tags: ["individual"]
+            },
+            {
+                src: "https://ik.imagekit.io/dewypole/matt-shaw-photoshoot/JUL_8466.jpg",
+                location: "Occidental Park",
+                tags: ["individual"]
+            },
+            {
+                src: "https://ik.imagekit.io/dewypole/matt-shaw-photoshoot/Untitled-1.jpg",
+                location: "Occidental Park",
+                tags: ["group"]
+            },
+            {
+                src: "https://ik.imagekit.io/dewypole/matt-shaw-photoshoot/JUL_8653.jpg",
+                location: "Occidental Park",
+                tags: ["individual"]
+            },
+            {
+                src: "https://ik.imagekit.io/dewypole/matt-shaw-photoshoot/JUL_8416.jpg",
+                location: "Occidental Park",
+                tags: ["individual"]
+            },
+            {
+                src: "https://ik.imagekit.io/dewypole/matt-shaw-photoshoot/JUL_8682.jpg",
+                location: "Occidental Park",
+                tags: ["group"]
+            },
+            {
+                src: "https://ik.imagekit.io/dewypole/matt-shaw-photoshoot/JUL_8486.jpg",
+                location: "Occidental Park",
+                tags: ["individual"]
+            },
+            {
+                src: "https://ik.imagekit.io/dewypole/matt-shaw-photoshoot/JUL_8640.jpg",
+                location: "Occidental Park",
+                tags: ["individual"]
+            },
+            {
+                src: "https://ik.imagekit.io/dewypole/matt-shaw-photoshoot/JUL_8644.jpg",
+                location: "Occidental Park",
+                tags: ["individual"]
+            },
+            {
+                src: "https://ik.imagekit.io/dewypole/matt-shaw-photoshoot/JUL_8636.jpg",
+                location: "Occidental Park",
+                tags: ["individual"]
+            },
+            {
+                src: "https://ik.imagekit.io/dewypole/matt-shaw-photoshoot/JUL_8651.jpg",
+                location: "Occidental Park",
+                tags: ["individual"]
+            },
+            {
+                src: "https://ik.imagekit.io/dewypole/matt-shaw-photoshoot/JUL_8655.jpg",
+                location: "Occidental Park",
+                tags: ["individual"]
+            },
+
+            //GASWORKS PARK
+            {
+                src: "https://ik.imagekit.io/dewypole/matt-shaw-photoshoot/JUL_9170.jpg",
+                location: "Gasworks Park",
+                tags: ["individual"]
+            },
+            {
+                src: "https://ik.imagekit.io/dewypole/matt-shaw-photoshoot/JUL_9167.jpg",
+                location: "Gasworks Park",
+                tags: ["individual"]
+            },
+            {
+                src: "https://ik.imagekit.io/dewypole/matt-shaw-photoshoot/JUL_9246.jpg",
+                location: "Gasworks Park",
+                tags: ["individual"]
+            },
+            {
+                src: "https://ik.imagekit.io/dewypole/matt-shaw-photoshoot/JUL_9247.jpg",
+                location: "Gasworks Park",
+                tags: ["individual"]
+            },
+        ]
+    }
+]
